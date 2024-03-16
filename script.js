5608c10fdc6c2bb4dbb2fa42c520d8678d148bb2
nasyaoy <122662286+nasyaoy@users.noreply.github.com>
2024-03-16 14:44:25 +0500

    promise

---
+++ script.js
@@ -0,0 +1,18 @@
+const url1 = "http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-019.jpg"
+const url2 = "http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-023.jpg"
+const url3 = "http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-09.jpg"
+
+const img = document.createElement('img');
+img.height = 200;
+img.src = url1
+document.body.append(img)
+
+const img2 = document.createElement('img');
+img2.height = 200;
+img2.src = url2
+document.body.append(img2)
+
+const img3 = document.createElement('img');
+img3.height = 200;
+img3.src = url3
+document.body.append(img3)
